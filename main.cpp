badc30caeae5f4fbb27f4c32446d4baa99e57b29
playbackthatradiohit <126272403+playbackthatradiohit@users.noreply.github.com>
2024-04-01 16:50:09 -0700

    Fix C++ standard style

--- main.cpp
+++ main.cpp
@@ -16,15 +16,15 @@ int main()
         cout << "Enter the page number: ";
         wstring page_num;
         wcin >> page_num;
-        if (std::stoi(page_num) <= 5 && std::stoi(page_num) >= 1)
+        if (stoi(page_num) <= 5 && stoi(page_num) >= 1)
         {
             wstring rundll32_params = L"shell32.dll,Control_RunDLL sysdm.cpl,," + page_num;
             HINSTANCE result = ShellExecuteW(nullptr, L"open", L"rundll32.exe", rundll32_params.c_str(), nullptr, SW_SHOW);
             if ((int)result <= 32) {
                 cerr << "\nError executing rundll32.exe.\nError code: " << (int)result << endl;
 
-                std::cout << "Press the enter/return key to exit..." << std::endl;
-                std::getchar();
+                cout << "Press the enter/return key to exit..." << endl;
+                getchar();
                 return 1;
             }
 
